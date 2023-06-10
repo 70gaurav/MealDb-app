2a3b638af859febea6ae4416aec1ce053ed40652
70gaurav <gk17265@gmail.com>
2023-06-10 22:02:38 +0530

    singlecategory

--- src/Singlecategory.jsx
+++ src/Singlecategory.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { singleCategoryData } from './Slice/Mealslice';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -10,7 +10,19 @@ function Singlecategory() {
 
   return (
     <div>
-      
+       <div className='category' >
+      {data.isLoading ? <div className="loader"></div> : ''}
+      <div className="category-container">
+      {
+        Singlecategory ? (
+          <div className="category-container">
+            <h1>{strMeal}</h1>
+            <img src={strMealThumb} alt="" />
+          </div>
+        ) : ("")
+      }
+      </div>
+    </div>
     </div>
   );
 }
